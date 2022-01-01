639fe317d5ed2920319654dac265b990bd271cbd
ToobaKhanam321 <86688363+ToobaKhanam321@users.noreply.github.com>
2022-01-01 05:56:58 +0500

    Update Projects.java

--- Projects.java
+++ Projects.java
@@ -10,8 +10,8 @@ import java.awt.event.*;
 import java.awt.*;
 import java.io.*;
 import java.lang.*;
-abstract class Write{   //it is an abstract class
-    abstract void Print();      //it is an abstract method
+abstract class Hostels{   //it is an abstract class
+    abstract void display();      //it is an abstract method
 }
 class Management extends Write implements ActionListener{    //it is our base class 
 JFrame jf;  //declare variable of JFrame
@@ -22,10 +22,10 @@ label11,label12,label13,label14,label15,label16,label17,label18,label19,label20
 ,label21,label22,label23,label24,label25,label26,label118,label119;
 
 //variables for JComboBox
-JComboBox jcb,jcb01,jcb02,jcb04,jcb05,jcb06,jcb07,jcb08,jcb1,jcb2,jcb3,jcb4,jcb5,jcb6,jcb7;
+JComboBox jcb,box01,box02,box04,box05,box06,box07,box08,box1,box09;
 //variables for JTextField
 
-JTextField txt01,txt02,txt03,txt04,txt09,txt009,txt010,txt1,txt2,txt3,txt4,txt5,txt6,txt7,txt8,txt9,txt10,txt11,txt12;
+JTextField txt01,t02,t03,t04,t09,t009,t010,t1,t2,t3,t4,t5,t6,t7,t8,t9,t10,t11,t12;
 JButton b1,b2;  //variables for JButton
 JPanel panel1;  //variables for JPanel
 
@@ -47,70 +47,70 @@ Management(){        //constructor
     label03 = new JLabel("Registration No.");
     label03.setBounds(10, 40, 120, 20);
     jf.add(label03);
-    txt03 = new JTextField();                      //create JTextField 
-    txt03.setBounds(120, 40, 140, 20);
-    txt03.addActionListener(this);
-    jf.add(txt03);
+    t03 = new JTextField();                      //create JTextField 
+    t03.setBounds(120, 40, 140, 20);
+    t03.addActionListener(this);
+    jf.add(t03);
     
     label2 = new JLabel("Student Name");
     label2.setBounds(10, 70, 120, 20);
     jf.add(label2);
-    txt01 = new JTextField();                      //create JTextField 
-    txt01.setBounds(120, 70, 140, 20);
-    txt01.addActionListener(this);
-    jf.add(txt01);
+    t01 = new JTextField();                      //create JTextField 
+    t01.setBounds(120, 70, 140, 20);
+    t01.addActionListener(this);
+    jf.add(t01);
     
     label101 = new JLabel("Class");
     label101.setBounds(10, 100, 100, 20);
     jf.add(label101);
     String [] options33 = {"Bachelors","Masters","M.Phil","Phd"};
-    jcb05 = new JComboBox(options33);
-    jcb05.setBounds(120, 100, 140, 20);
-    jcb05.addActionListener(this);
-    jf.add(jcb05);
+    box05 = new JComboBox(options33);
+   box05.setBounds(120, 100, 140, 20);
+    box05.addActionListener(this);
+    jf.add(box05);
     
     label6 = new JLabel("Department");
     label6.setBounds(10, 130, 100, 20);
     jf.add(label6);
     String [] options1 = {"Food Science","Microbiology","Pharmacy","Computer Science","Software Engineering",
     "Bussiness Adminstration","Chemistry","Biotechnology"};
-    jcb1 = new JComboBox(options1);
-    jcb1.setBounds(120, 130, 140, 20);
-    jcb1.addActionListener(this);
-    jf.add(jcb1);
+    box1 = new JComboBox(options1);
+    box1.setBounds(120, 130, 140, 20);
+    box1.addActionListener(this);
+    jf.add(box1);
        
     label101 = new JLabel("Batch");
     label101.setBounds(10, 160, 100, 20);
     jf.add(label101);
     String [] options3 = {"2021","2020","2019","2018","2017"};
-    jcb02 = new JComboBox(options3);
-    jcb02.setBounds(120, 160, 140, 20);
-    jcb02.addActionListener(this);
-    jf.add(jcb02);
+    box02 = new JComboBox(options3);
+    box02.setBounds(120, 160, 140, 20);
+    box02.addActionListener(this);
+    jf.add(box02);
     
     label5 = new JLabel("Gender");
     label5.setBounds(10,190,100, 20);
     jf.add(label5);
-    txt04 = new JTextField();
-    txt04.setBounds(120, 190, 140, 20);
-    txt04.addActionListener(this);
-    jf.add(txt04);
+    t04 = new JTextField();
+    t04.setBounds(120, 190, 140, 20);
+    t04.addActionListener(this);
+    jf.add(t04);
     
     label3 = new JLabel("Contact Number");
     label3.setBounds(10, 220, 120, 20);
     jf.add(label3);
-    txt1 = new JTextField();                             //create JTextField 
-    txt1.setBounds(120, 220, 140, 20);
-    txt1.addActionListener(this);
-    jf.add(txt1);
+    t1 = new JTextField();                             //create JTextField 
+    t1.setBounds(120, 220, 140, 20);
+    t1.addActionListener(this);
+    jf.add(t1);
     
     label33 = new JLabel("CNIC Number");
     label33.setBounds(10, 250, 120, 20);
     jf.add(label33);
-    txt02 = new JTextField();                            //create JTextField 
-    txt02.setBounds(120, 250, 140, 20);
-    txt02.addActionListener(this);
-    jf.add(txt02);
+    t02 = new JTextField();                            //create JTextField 
+    t02.setBounds(120, 250, 140, 20);
+    t02.addActionListener(this);
+    jf.add(t02);
     
     label11 = new JLabel("PARENTS DETAILS");
     label11.setBounds(10, 280, 250, 20);
@@ -121,52 +121,52 @@ Management(){        //constructor
     label5 = new JLabel("Father Name");
     label5.setBounds(10,310,100, 20);
     jf.add(label5);
-    txt2 = new JTextField();
-    txt2.setBounds(120, 310, 140, 20);
-    txt2.addActionListener(this);
-    jf.add(txt2);
+    t2 = new JTextField();
+    t2.setBounds(120, 310, 140, 20);
+    t2.addActionListener(this);
+    jf.add(t2);
     
     label7 = new JLabel("Parents Contact1");
     label7.setBounds(10, 340, 100, 20);
     jf.add(label7);
-    txt4 = new JTextField();
-    txt4.setBounds(120, 340, 140, 20);
-    txt4.addActionListener(this);
-    jf.add(txt4);
+    t4 = new JTextField();
+    t4.setBounds(120, 340, 140, 20);
+    t4.addActionListener(this);
+    jf.add(t4);
     
     label8 = new JLabel("Parents Contact2");
     label8.setBounds(10, 370, 100, 20);
     jf.add(label8);
-    txt5 = new JTextField();
-    txt5.setBounds(120, 370, 140, 20);
-    txt5.addActionListener(this);
-    jf.add(txt5);
+    t5 = new JTextField();
+    t5.setBounds(120, 370, 140, 20);
+    t5.addActionListener(this);
+    jf.add(t5);
     
     label9 = new JLabel("Father Occupaion");
     label9.setBounds(10, 400, 100, 20);
     jf.add(label9);
-    txt6 = new JTextField();
-    txt6.setBounds(120, 400, 140, 20);
-    txt6.addActionListener(this);
-    jf.add(txt6);
+    t6 = new JTextField();
+    t6.setBounds(120, 400, 140, 20);
+    t6.addActionListener(this);
+    jf.add(t6);
     
     label10 = new JLabel("City");
     label10.setBounds(10, 430, 100, 20);
     jf.add(label10);
     String [] options2 = {"Karachi","Hyderabad","Faislabad","Islamabad","Lahore",
     "Quetta","Muree","Gawadar"};
-    jcb01 = new JComboBox(options2);
-    jcb01.setBounds(120, 430, 140, 20);
-    jcb01.addActionListener(this);
+    box01 = new JComboBox(options2);
+    box01.setBounds(120, 430, 140, 20);
+    box01.addActionListener(this);
     jf.add(jcb01);
     
     label18 = new JLabel("Address");
     label18.setBounds(10, 460, 100, 20);
     jf.add(label18);    
-    txt009 = new JTextField();
-    txt009.setBounds(120, 460, 140, 20);
-    txt009.addActionListener(this);
-    jf.add(txt009);
+    t009 = new JTextField();
+    t009.setBounds(120, 460, 140, 20);
+    t009.addActionListener(this);
+    jf.add(t009);
     
     label15 = new JLabel("ROOM DETAILS");
     label15.setBounds(300, 10, 250, 20);
@@ -178,28 +178,28 @@ Management(){        //constructor
     label16.setBounds(300, 40, 100, 20);
     jf.add(label16); 
     String [] options04 = {"Jinnah Block","Allama Iqbal Block","Fatima Jinnah Block"};
-    jcb04 = new JComboBox(options04);
-    jcb04.setBounds(410, 40, 140, 20);
-    jcb04.addActionListener(this);
-    jf.add(jcb04);
+    box04 = new JComboBox(options04);
+    box04.setBounds(410, 40, 140, 20);
+    box04.addActionListener(this);
+    jf.add(box04);
     
     label16 = new JLabel("Room No.");
     label16.setBounds(300, 70, 100, 20);
     jf.add(label16); 
     String [] options06 = {"1","2","3","4","5","6","7","8","9","10"};
-    jcb06 = new JComboBox(options06);
-    jcb06.setBounds(410, 70, 140, 20);
-    jcb06.addActionListener(this);
-    jf.add(jcb06);
+    box06 = new JComboBox(options06);
+    box06.setBounds(410, 70, 140, 20);
+    box06.addActionListener(this);
+    jf.add(box06);
     
     label17 = new JLabel("Room Location");
     label17.setBounds(300, 100, 100, 20);
     jf.add(label17);    
     String [] options07 = {"Ground Floor","First Floor","Second Floor","Third Floor"};
-    jcb07 = new JComboBox(options07);
-    jcb07.setBounds(410, 100, 140, 20);
-    jcb07.addActionListener(this);
-    jf.add(jcb07);
+    box07 = new JComboBox(options07);
+    box07.setBounds(410, 100, 140, 20);
+    box07.addActionListener(this);
+    jf.add(box07);
     
     label19 = new JLabel("FACILITIES");
     label19.setBounds(300, 130, 250, 20);
@@ -211,10 +211,10 @@ Management(){        //constructor
     label17.setBounds(300, 160, 100, 20);
     jf.add(label17);    
     String [] options08 = {"Single","Double"};
-    jcb08 = new JComboBox(options08);
-    jcb08.setBounds(410, 160, 140, 20);
-    jcb08.addActionListener(this);
-    jf.add(jcb08);
+    box08 = new JComboBox(options08);
+    box08.setBounds(410, 160, 140, 20);
+    box08.addActionListener(this);
+    jf.add(box08);
     
     //create Checkbox
     JCheckBox checkBox1 = new JCheckBox("Study Tables are available in the Room.");  
@@ -239,10 +239,10 @@ Management(){        //constructor
     label18 = new JLabel("Name");
     label18.setBounds(300, 290, 100, 20);
     jf.add(label18);    
-    txt9 = new JTextField();
-    txt9.setBounds(410, 290, 140, 20);
-    txt9.addActionListener(this);
-    jf.add(txt9);
+    t9 = new JTextField();
+    t9.setBounds(410, 290, 140, 20);
+    t9.addActionListener(this);
+    jf.add(t9);
     
     label118 = new JLabel("Relation");
     label118.setBounds(300, 320, 100, 20);
@@ -255,10 +255,10 @@ Management(){        //constructor
     label119 = new JLabel("Contac No");
     label119.setBounds(300, 350, 100, 20);
     jf.add(label119);    
-    txt010 = new JTextField();
-    txt010.setBounds(410, 350, 140, 20);
-    txt010.addActionListener(this);
-    jf.add(txt010);
+    t010 = new JTextField();
+    t010.setBounds(410, 350, 140, 20);
+    t010.addActionListener(this);
+    jf.add(t010);
     
     label22 = new JLabel("ROOM RENT");
     label22.setBounds(300, 380, 250, 20);
@@ -269,22 +269,22 @@ Management(){        //constructor
     label24 = new JLabel("Rent");
     label24.setBounds(300, 410, 100, 20);
     jf.add(label24);    
-    txt11 = new JTextField("0");
-    txt11.setBounds(410, 410, 140, 20);
-    jf.add(txt11);
+    t11 = new JTextField("0");
+    t11.setBounds(410, 410, 140, 20);
+    jf.add(t11);
     label25 = new JLabel("Payment method");
     label25.setBounds(300, 440, 100, 20);
     jf.add(label25);    
     String [] options8 = {"Cash","Online"};
-    jcb7 = new JComboBox(options8);
-    jcb7.setBounds(410, 440, 140, 20);
-    jf.add(jcb7);   
+    box09 = new JComboBox(options8);
+    box09.setBounds(410, 440, 140, 20);
+    jf.add(box09);   
     label26 = new JLabel("Total Amount");
     label26.setBounds(300, 480, 100, 20);
     jf.add(label26);    
-    txt12 = new JTextField("0");
-    txt12.setBounds(410, 480, 140, 20);
-    jf.add(txt12);
+    t12 = new JTextField("0");
+    t12.setBounds(410, 480, 140, 20);
+    jf.add(t12);
 
     b1 = new JButton("Calculate Total");          //create JButton with specified text
     b1.setBounds(300, 510, 120, 40);
@@ -301,29 +301,29 @@ Management(){        //constructor
     jf.setVisible(true);
 }
 public void actionPerformed(ActionEvent e){     //override method of ActionListener interface
-    Registration = txt03.getText();
-    name= txt01.getText();                      //to know what is written in text field
-    classs=(String) jcb05.getSelectedItem();
-    gender=txt04.getText();                     //to know what is written in text field
-    cnic = txt02.getText();
-    department = (String) jcb01.getSelectedItem();
-    batch = (String) jcb02.getSelectedItem();
+    Registration = t03.getText();
+    name= t01.getText();                      //to know what is written in text field
+    classs=(String) box05.getSelectedItem();
+    gender=t04.getText();                     //to know what is written in text field
+    cnic = t02.getText();
+    department = (String) box01.getSelectedItem();
+    batch = (String) box02.getSelectedItem();
     father = txt2.getText();
-    p_contact1 = txt4.getText();
-    p_contact2 = txt5.getText();
-    f_occupation = txt6.getText();
-    address = txt9.getText();
-    addressN=txt009.getText();
-    c_city = (String) jcb1.getSelectedItem();
-    block = (String) jcb04.getSelectedItem();
-    roomNum = (String) jcb06.getSelectedItem();
-    location = (String) jcb07.getSelectedItem();
-    bed = (String) jcb08.getSelectedItem();
-    relation=txt09.getText();
-    emerCont=txt010.getText();
+    p_contact1 = t4.getText();
+    p_contact2 = t5.getText();
+    f_occupation = t6.getText();
+    address = t9.getText();
+    addressN=t009.getText();
+    c_city = (String) box1.getSelectedItem();
+    block = (String) box04.getSelectedItem();
+    roomNum = (String) box06.getSelectedItem();
+    location = (String) box07.getSelectedItem();
+    bed = (String) box08.getSelectedItem();
+    relation=t09.getText();
+    emerCont=t010.getText();
     rent=txt11.getText();
-    pay_method = (String)jcb7.getSelectedItem();
-    total_amt=txt12.getText();
+    pay_method = (String)box09.getSelectedItem();
+    total_amt=t12.getText();
    
     String str = e.getActionCommand();
     if (str == "Confirm Room"){    //checking either confirm order or calculate total button is pressed
@@ -349,11 +349,11 @@ public void actionPerformed(ActionEvent e){     //override method of ActionListe
         else if(block == "Allama Iqbal"){num3 = 4000;}
         else{ num3 =2000;}
         num4 = (num1+num2+num3);                        //calculate total
-        String s1 = String.valueOf(num4);
-        txt12.setText(s1);    
+        String str1 = String.valueOf(num4);
+        txt12.setText(str1);    
     
 }}
-void Print(){                                           //override method of abstract class "Write"
+void display(){                                           //override method of abstract class "Write"
     try{                                                //try to run the following code
         File file = new File("Order Details.txt");      //create object of file
         FileWriter fr = new FileWriter(file, true);     //create object of FileWriter
@@ -361,53 +361,53 @@ void Print(){                                           //override method of abs
         PrintWriter pw = new PrintWriter(br);           //create object of PrintWriter
         
         //we use above classes to append data in the end of file rather than over write the previous data
-        String s1 = "Registration no.: "+Registration;     //making String to write in file
-        String s2 = "Student Name: "+name;
-        String s3 = "Student Class: "+classs;
-        String s4 = "Student Department: "+c_city;
-        String s5 = "Student Batch: "+batch;
-        String s6 = "Student Gender: "+gender;
-        String s7 = "Student CNIC: "+cnic;
-        String s8 = "Student Father: "+father;
-        String s9 = "Parent Contact1: "+p_contact1;
-        String s10 = "Parent Contact2: "+p_contact2;
-        String s11 = "Father Occupation: "+f_occupation;
-        String s12 = "City: "+department;
-        String s25 = "Address: "+addressN;
-        String s13 = "Student Hostel Name: "+block;
-        String s14 = "Student Room No: "+roomNum;
-        String s15 = "Student Room Location: "+location;
-        String s16 = "Student Bed: "+bed;
-        String s28 ="Emergency Contact: "+address;
-        String s29="Student Relation: "+relation;
-        String s30="Student Emergency Contact No.: "+emerCont;
-        String s31="Room Rent: "+rent;
-        String s32="Payment Method: "+pay_method;
-        String s17 = "Total Amount: "+num4;
+        String str1 = "Registration no.: "+Registration;     //making String to write in file
+        String str2 = "Student Name: "+name;
+        String str3 = "Student Class: "+classs;
+        String str4 = "Student Department: "+c_city;
+        String str5 = "Student Batch: "+batch;
+        String str6 = "Student Gender: "+gender;
+        String str7 = "Student CNIC: "+cnic;
+        String str8 = "Student Father: "+father;
+        String str9 = "Parent Contact1: "+p_contact1;
+        String str10 = "Parent Contact2: "+p_contact2;
+        String str11 = "Father Occupation: "+f_occupation;
+        String str12 = "City: "+department;
+        String str25 = "Address: "+addressN;
+        String str13 = "Student Hostel Name: "+block;
+        String str14 = "Student Room No: "+roomNum;
+        String str15 = "Student Room Location: "+location;
+        String str16 = "Student Bed: "+bed;
+        String str28 ="Emergency Contact: "+address;
+        String str29="Student Relation: "+relation;
+        String str30="Student Emergency Contact No.: "+emerCont;
+        String str31="Room Rent: "+rent;
+        String str32="Payment Method: "+pay_method;
+        String str17 = "Total Amount: "+num4;
         
-        pw.println(s1);     //write data in file
-        pw.println(s2);
-        pw.println(s3);
-        pw.println(s4);
-        pw.println(s5);
-        pw.println(s6);
-        pw.println(s7);
-        pw.println(s8);
-        pw.println(s9);
-        pw.println(s10);
-        pw.println(s11);
-        pw.println(s12);
-        pw.println(s25);
-        pw.println(s13);
-        pw.println(s14);
-        pw.println(s15);
-        pw.println(s16);
-        pw.println(s28);
-        pw.println(s29);
-        pw.println(s30);
-        pw.println(s31);
-        pw.println(s32);
-        pw.println(s17);
+        pw.println(str1);     //write data in file
+        pw.println(str2);
+        pw.println(str3);
+        pw.println(str4);
+        pw.println(str5);
+        pw.println(str6);
+        pw.println(str7);
+        pw.println(str8);
+        pw.println(str9);
+        pw.println(str10);
+        pw.println(str11);
+        pw.println(str12);
+        pw.println(str25);
+        pw.println(str13);
+        pw.println(str14);
+        pw.println(str15);
+        pw.println(str16);
+        pw.println(str28);
+        pw.println(str29);
+        pw.println(str30);
+        pw.println(str31);
+        pw.println(str32);
+        pw.println(str17);
         pw.println();
         pw.flush();     //enforce all data to write in file
         pw.close();     //closes PrintWriter
